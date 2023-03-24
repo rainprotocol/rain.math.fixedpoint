8dcd1707ccefe9bfa574190417f362e06d076d25
thedavidmeister <thedavidmeister@gmail.com>
2023-03-24 17:02:58 +0400

    scale by

--- test/LibFixedPointScale.t.sol
+++ test/LibFixedPointScale.t.sol
@@ -5,12 +5,16 @@ import "forge-std/Test.sol";
 import "../src/LibFixedPointScale.sol";
 
 contract LibFixedPointScaleTest is Test {
+    function scaleUp(uint256 a_, uint256 scaleBy_) internal returns (uint256) {
+        return a_ * (10 ** scaleBy_);
+    }
+
     function testScaleDownRoundDown(uint256 a_, uint256 scaleDownBy_, uint256 rounding_) public {
         vm.assume(rounding_ != ROUND_UP);
         vm.assume(scaleDownBy_ <= MAX_RESCALE_OOMS);
 
         uint256 b_ = 10 ** scaleDownBy_;
-        uint256 c_  = a_ / b_;
+        uint256 c_ = a_ / b_;
 
         assertEq(c_, LibFixedPointScale.scaleDown(a_, scaleDownBy_, rounding_));
     }
@@ -33,5 +37,46 @@ contract LibFixedPointScaleTest is Test {
 
         vm.expectRevert(stdError.arithmeticError);
         LibFixedPointScale.scaleDown(a_, scaleDownBy_, rounding_);
+    }
+
+    function testScaleDown0(uint256 a_, uint256 rounding_) public {
+        assertEq(a_, LibFixedPointScale.scaleDown(a_, 0, rounding_));
+    }
+
+    function testScaleBy0(uint256 a_, uint256 rounding_) public {
+        assertEq(a_, LibFixedPointScale.scaleBy(a_, 0, rounding_));
+    }
+
+    function testScaleByUp(uint256 a_, int8 scaleBy_, uint256 rounding_) public {
+        vm.assume(scaleBy_ > 0);
+        vm.assume(uint8(scaleBy_) <= MAX_RESCALE_OOMS);
+
+        unchecked {
+            uint256 b_ = 10 ** uint8(scaleBy_);
+            uint256 c_ = a_ * b_;
+            vm.assume(c_ / b_ == a_);
+        }
+
+        assertEq(
+            scaleUp(a_, uint256(uint8(scaleBy_))),
+            LibFixedPointScale.scaleBy(a_, scaleBy_, rounding_)
+        );
+    }
+
+    function testScaleByDown(uint256 a_, int8 scaleBy_, uint256 rounding_) public {
+        vm.assume(scaleBy_ < 0);
+        vm.assume(stdMath.abs(scaleBy_) <= MAX_RESCALE_OOMS);
+
+        assertEq(
+            LibFixedPointScale.scaleBy(a_, scaleBy_, rounding_),
+            LibFixedPointScale.scaleDown(a_, stdMath.abs(scaleBy_), rounding_)
+        );
+    }
+
+    function testScaleByOverflow(uint256 a_, int8 scaleBy_, uint256 rounding_) public {
+        vm.assume(stdMath.abs(scaleBy_) > MAX_RESCALE_OOMS);
+
+        vm.expectRevert(stdError.arithmeticError);
+        LibFixedPointScale.scaleBy(a_, scaleBy_, rounding_);
     }
 }
