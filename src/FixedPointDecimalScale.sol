f87e033c9274dec75849354346141a93188696c1
thedavidmeister <thedavidmeister@gmail.com>
2023-03-25 15:08:00 +0400

    unchecked scale down rounding

--- src/FixedPointDecimalScale.sol
+++ src/FixedPointDecimalScale.sol
@@ -96,12 +96,16 @@ library FixedPointDecimalScale {
     function scaleDown(uint256 a_, uint256 scaleDownBy_, uint256 rounding_) internal pure returns (uint256) {
         uint256 b_ = 10 ** scaleDownBy_;
         uint256 scaled_ = a_ / b_;
-        // Intentionally doing a divide before multiply here to detect the need
-        // to round up.
-        //slither-disable-next-line divide-before-multiply
-        if (rounding_ == ROUND_UP && a_ != scaled_ * b_) {
-            scaled_ += 1;
+
+        unchecked {
+            // Intentionally doing a divide before multiply here to detect the need
+            // to round up.
+            //slither-disable-next-line divide-before-multiply
+            if (rounding_ == ROUND_UP && a_ != scaled_ * b_) {
+                scaled_ += 1;
+            }
         }
+
         return scaled_;
     }
 
